c6444f036c27e3de3269be31da6cdb1a7b6be9af
ttiimmothy <dinosauli2006@gmail.com>
2023-11-12 00:29:31 -0500

    fix: change the api path

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,15 +1,19 @@
-import { AxiosApiClientBuilder } from '..';
-import { User, AuthenticateResponse } from './authType';
+import { AxiosApiClientBuilder } from "..";
+import { User, AuthenticateResponse } from "./authType";
 
 const apiClient = new AxiosApiClientBuilder()
-	.withResourceName('/auth')
-	.withCredentials(true)
-	.build();
+  .withResourceName("/user/auth")
+  .withCredentials(true)
+  .build();
 
-export const postUserRegister = async (user: User): Promise<AuthenticateResponse> => {
-	return apiClient.post('/register', user);
+export const postUserRegister = async (
+  user: User
+): Promise<AuthenticateResponse> => {
+  return apiClient.post("/register", user);
 };
 
-export const postUserAuth = async (user: User): Promise<AuthenticateResponse> => {
-	return apiClient.post('/authenticate', user);
+export const postUserAuth = async (
+  user: User
+): Promise<AuthenticateResponse> => {
+  return apiClient.post("/login", user);
 };
