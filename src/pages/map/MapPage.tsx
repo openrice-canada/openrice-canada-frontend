a78115135a70dd0e3eb7fa19a4856193617da8ec
ttiimmothy <dinosauli2006@gmail.com>
2023-11-13 02:04:57 -0500

    feat: add thunk logic in map page

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -4,25 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, IRootState } from "../../store";
 
 import MapComponent from "../../components/map/MapComponent";
-<<<<<<< Updated upstream
-import { getRestaurantList } from "../../api/restaurant/restaurantApiIndex";
-import { Restaurant } from "../../api/restaurant/RestaurantType";
-
-const MapPage = () => {
-  const [restaurantList, setRestaurantList] = useState<Restaurant[]>([]);
-
-  const fetchRestaurantList = async () => {
-    const data = await getRestaurantList({});
-    setRestaurantList(data);
-  };
-=======
 
 const MapPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const restaurants = useSelector(
     (state: IRootState) => state.restaurant.restaurants
   );
->>>>>>> Stashed changes
 
   useEffect(() => {
     const fetchRestaurantList = async () => {
