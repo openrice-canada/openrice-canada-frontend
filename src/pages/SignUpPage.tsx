bd870f9f2bdcba52c123514d1f3f2c4d3394fa61
ttiimmothy <dinosauli2006@gmail.com>
2023-12-04 03:24:57 -0500

    feat: update profile page, closes #36
    co-authored-by: wingck <ckwwingo@gmail.com>
    co-authored-by: marttsang2 <tsangmartin1225@gmail.com>

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,30 +1,48 @@
-import { useEffect } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { closeSnackbar, enqueueSnackbar } from "notistack";
 
 import { AppDispatch, IRootState } from "../store";
-import { registerThunk, updateMessage } from "../redux/auth/authSlice";
+import {
+  registerThunk,
+  updateMessage,
+  updateRegisterStatus,
+} from "../redux/auth/authSlice";
 import TextInput from "../components/utils/inputs/TextInput";
+import FileInput from "../components/utils/inputs/FileInput";
+import { fileTypeToExtension } from "../utils/fileTypeToExtension";
+import { register } from "../api/auth/authApiIndex";
+import { uploadUserProfilePicture } from "../utils/uploadImageService";
+
+interface RegisterForm {
+  username: string;
+  email: string;
+  password: string;
+  photo?: any;
+}
 
 const SignUpPage = () => {
   const navigate = useNavigate();
-  const { handleSubmit, control } = useForm();
+  const { handleSubmit, control } = useForm<RegisterForm>({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      photo: "",
+    },
+  });
+  const [selectedImage, setSelectedImage] = useState<
+    string | ArrayBuffer | null
+  >(null);
 
   const dispatch = useDispatch<AppDispatch>();
   const registerSuccess = useSelector(
     (state: IRootState) => state.auth.registerSuccess
   );
   const message = useSelector((state: IRootState) => state.auth.message);
-
-  const userRegister = async (user: {
-    email: string;
-    username: string;
-    password: string;
-  }) => {
-    dispatch(registerThunk(user));
-  };
 
   useEffect(() => {
     if (registerSuccess) {
@@ -52,38 +70,80 @@ const SignUpPage = () => {
     }, 2000);
   }, [dispatch, message]);
 
+  const userRegister = async (user: RegisterForm) => {
+    if (user.photo) {
+      const res = await register(
+        { username: user.username, email: user.email, password: user.password },
+        fileTypeToExtension[user.photo.type]
+      );
+
+      if (res.message) {
+        dispatch(updateRegisterStatus(false));
+        dispatch(updateMessage(res.message));
+      }
+
+      if (res.user && res.user.user_id) {
+        dispatch(updateRegisterStatus(true));
+        sessionStorage.setItem("jwt", res.token as string);
+        await uploadUserProfilePicture(
+          user.photo,
+          res.user.user_id,
+          fileTypeToExtension[user.photo.type]
+        );
+      }
+    } else {
+      dispatch(
+        registerThunk({
+          username: user.username,
+          email: user.email,
+          password: user.password,
+        })
+      );
+    }
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let file;
+    if (e.target.files) {
+      file = e.target.files[0];
+    }
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setSelectedImage(null);
+    }
+  };
+
   return (
     <form
       className="h-screen flex flex-col gap-6 justify-center max-w-sm mx-auto px-4"
-      onSubmit={handleSubmit((user) =>
-        userRegister(
-          user as { email: string; username: string; password: string }
-        )
-      )}
+      onSubmit={handleSubmit((user) => userRegister(user))}
     >
-      <p className="text-3xl font-bold">Sign up for Openrice</p>
-      <p>
+      <div className="text-3xl font-bold">Sign up for Openrice</div>
+      <div>
         By continuing, you agree to Openrice’s Terms of Service and acknowledge
         Openrice’s Privacy Policy.
-      </p>
-      <Controller
-        name="email"
-        control={control}
-        defaultValue={""}
-        render={({ field }) => (
-          <TextInput
-            label="Email"
-            type="email"
-            placeholder="Enter your email"
-            value={field.value}
-            onChange={field.onChange}
-          />
-        )}
-      />
+      </div>
+      {selectedImage && (
+        <div>
+          <div className="text-sm font-semibold">Preview</div>
+          <div className="mb-1 shadow-md rounded-md">
+            <img
+              src={selectedImage as string}
+              alt="Preview"
+              className="w-full h-auto rounded-md"
+            />
+          </div>
+        </div>
+      )}
       <Controller
         name="username"
         control={control}
-        defaultValue={""}
         render={({ field }) => (
           <TextInput
             label="Username"
@@ -95,9 +155,21 @@ const SignUpPage = () => {
         )}
       />
       <Controller
+        name="email"
+        control={control}
+        render={({ field }) => (
+          <TextInput
+            label="Email"
+            type="email"
+            placeholder="Enter your email"
+            value={field.value}
+            onChange={field.onChange}
+          />
+        )}
+      />
+      <Controller
         name="password"
         control={control}
-        defaultValue={""}
         render={({ field }) => (
           <TextInput
             label="Password"
@@ -105,6 +177,25 @@ const SignUpPage = () => {
             placeholder="Enter your password"
             value={field.value}
             onChange={field.onChange}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="photo"
+        render={({ field }) => (
+          <FileInput
+            onChange={(e) => {
+              if (e.target.files) {
+                const selectedFile = e.target.files[0];
+                field.onChange(selectedFile);
+                handleImageChange(e);
+              }
+            }}
+            label="Profile Picture"
+            type="file"
+            className="form-control"
+            placeholder=""
           />
         )}
       />
